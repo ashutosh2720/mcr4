457a71ce596826d0b364edfc08c2e2128317ef6c
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-06-24 16:05:33 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dis from "./pages/Dis";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-black text-white">Hello world!</h1>
+      <Routes>
+        <Route path="/dis" element={<Dis />} />
+      </Routes>
     </>
   );
 }
