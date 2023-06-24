0328f86f8bf57a4f6dc8f81ee9aa3c8fd7e69dfa
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-06-24 15:55:17 +0530

    initial commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
       <h1 className="text-3xl font-bold underline bg-black text-white">
         Hello world!
       </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
