3986074400de66705778187f80e89f7c10a2e1e5
ASHUTOSH SINGH <ashutoshsinghvns95@gmail.com>
2023-06-24 16:02:02 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-black text-white">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline bg-black text-white">Hello world!</h1>
     </>
   );
 }
